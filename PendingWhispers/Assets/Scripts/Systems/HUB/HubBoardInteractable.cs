76eaf31bc408081aebac622c5642aa0394f87b06
PaulaBGM <borrasgarciap@gmail.com>
2026-09-22 16:38:26 +0200

    Revert "Implement Hub board with UI Toolkit"
    This reverts commit 0f596d03f75f88c0454d8f63e82141ee2dc04c9f.

--- PendingWhispers/Assets/Scripts/Systems/HUB/HubBoardInteractable.cs
+++ PendingWhispers/Assets/Scripts/Systems/HUB/HubBoardInteractable.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 [RequireComponent(typeof(Collider2D))]
 public class HubBoardInteractable : MonoBehaviour, IInteractable
 {
-    [SerializeField] private HubBoardUIToolkitController boardPanel;
+    [SerializeField] private CaseBoardPanelUI boardPanel;
 
     public void Interact(PlayerController_Actions player)
     {
